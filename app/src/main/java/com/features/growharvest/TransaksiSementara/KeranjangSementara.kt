6c0b1aab841b0a8f713fb1f050efd62277339e42
faturdzaky7124 <124643459+faturdzaky7124@users.noreply.github.com>
2023-12-25 12:09:20 +0700

    Menghapus Transaksi Sementara (Tidak Dibutuhkan)

--- app/src/main/java/com/features/growharvest/TransaksiSementara/KeranjangSementara.kt
+++
@@ -1,8 +0,0 @@
-package com.features.growharvest.TransaksiSementara
-
-data class KeranjangSementara(
-    val nama_produk: String,
-    val harga_produk: Int,
-    val jumlah_produk: Int
-) {
-}
--- app/src/main/java/com/features/growharvest/TransaksiSementara/ProdukSementara.kt
+++
@@ -1,7 +0,0 @@
-package com.features.growharvest.TransaksiSementara
-
-data class ProdukSementara(
-    val name: String,
-    val price: Int,
-    val gambar: Int,
-    val stock: Int)
